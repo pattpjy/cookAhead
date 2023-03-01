a2e12683003ae0074b25faacc26cc9fb2184cca0
Patt Sookmark <pijiya@sookmark.com>
2023-02-28 21:37:47 -0700

    Refactor fetch call

--- cook_ahead/src/App.js
+++ cook_ahead/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
@@ -6,7 +7,7 @@ const App = () => {
   const options = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "3174349b86msh3f56cf93786160ep1bcf87jsn6808a68eebaf",
+      "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
       "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
     },
   };
@@ -14,11 +15,10 @@ const App = () => {
   // const URL = "https://random.dog/woof.json";
   const getRandomDog = async () => {
     const response = await fetch(
-      "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=5",
+      "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=4",
       options
     );
     const body = await response.json();
-    console.log(body.recipes[0].title);
 
     return setUrl(body.recipes[0].title);
   };
@@ -31,7 +31,6 @@ const App = () => {
     <div className="App">
       <div className="main-board">
         <button onClick={onClickHandler}> Click me </button>
-        {url}
       </div>
     </div>
   );
