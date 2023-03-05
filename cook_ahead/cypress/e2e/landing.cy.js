bbb5637299d19bffdb98a8e1578ef61632525165
Patt Sookmark <pijiya@sookmark.com>
2023-03-05 09:31:37 -0700

    Add more test

--- cook_ahead/cypress/e2e/landing.cy.js
+++ cook_ahead/cypress/e2e/landing.cy.js
@@ -4,34 +4,31 @@ describe("Landing Page", () => {
   });
   describe("When landing page render correctly", () => {
     it("should have two buttons and nav bar", () => {
-      cy.get("#root > main > div > div > button:nth-child(1)").should(
-        "contain",
-        "Get Meal Ideas"
-      );
+      //add NAV bar testing
+      cy.get(".header-nav").should("contain", "My Meal Plan");
+      cy.get(".fa-duotone").should("be.visible");
+      cy.get(".get-ideas").should("contain", "Get Meal Ideas");
       cy.get(".view-weekly").should("contain", "See My Weekly Meals");
     });
     it("should render recipes card correctly when the generate button was clicked", () => {
-      // cy.intercept(
-      //   {
-      //     method: "GET",
-      //     url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=4",
-      //   },
-      //   { fixture: "recipes" }
-      // );
-      //add fixture
-      //title of recipes,
-      cy.get("#root > main > div > div > button:nth-child(1)").click();
+      cy.intercept(
+        {
+          method: "GET",
+          url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=4",
+        },
+        { fixture: "recipes" }
+      );
+      cy.get(".get-ideas").click();
       cy.get(".display-random > :nth-child(1)")
         .should("be.visible")
-        .contains("h3", "Ramen Chicken Noodle");
+        .contains(".card-title", "Ramen Chicken Noodle");
       //image
       cy.get(".display-random > :nth-child(1) > button").should("be.visible");
 
       //button for add to planner
     });
     it("should have correct link in the weekly meal plan button", () => {
-      cy.get(".view-weekly").click();
-      cy.url().should("include", "/weekly-plan");
+      cy.get(".view-weekly").click().url().should("include", "/weekly-plan");
     });
   });
 });
